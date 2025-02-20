5d32f4d949d94d6a1e617845882101d8c8ab37e2
sojeong0302 <01duqrl@naver.com>
2025-02-20 23:20:25 +0900

    feat: Header 컴포넌트 생성 #3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import MainPage from './page/MainPage.js';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-    return <></>;
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route path='/' element={<MainPage />} />
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
 export default App;
---
+++ src/component/Header/index.js
@@ -0,0 +1,5 @@
+function Header() {
+    return <>ImPro</>;
+}
+
+export default Header;
